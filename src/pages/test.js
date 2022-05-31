4f4d875c2b93bc79131b1e3751fda6b1f1699960
2himitu <hmhnes@gmail.com>
2022-05-31 13:06:44 +0900

    test오타수정

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { Like } from "gatsby";
+import { Link } from "gatsby";
 
 function testPage() {
   return (
     <main>
       <title> 테스트 페이지</title>
       <h1> 테스트 페이지입니다.</h1>
-      <Like to="/about"></Like>
+      <Link to="/about"></Link>
     </main>
   );
 }
